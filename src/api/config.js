5940eb88c40bbe3b54604671da61765d0b7c0097
GuilleFeru <guillelf@gmail.com>
2017-10-02 15:16:51 -0300

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+  appname: 'platzimusic',
+  apiKey: '3c0a47a52d3ebb41586714c1f078f81e',
+  secret: '771dd5cc64822ff349ba6fdc47addf1b',
+  registeredTo: 'GuilleFerru'
+}
+
+// Application name	platzimusic
+// API key	3c0a47a52d3ebb41586714c1f078f81e
+// Shared secret	771dd5cc64822ff349ba6fdc47addf1b
+// Registered to	GuilleFerru
---
+++ src/components/Artist.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <li class="artist">
+    <h2><a v-bind:href="artist.url" target="_blank">{{artist.name}}</a></h2>
+   <img :src="artist.image[2]['#text']" alt="">
+ </li>
+</template>
+
+<script>
+export default {
+  name : 'artist',
+  props: ['artist']
+
+
+}
+</script>
+
+<style lang="css" scoped>
+li.artist{
+  display: block;
+  margin: 10px 0;
+}
+</style>
